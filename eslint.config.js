61acf5dfaed03a18f598cdb0b72b0fb825405f03
Forrest Wilkins <ethanforrestwilkins@gmail.com>
2026-05-05 18:49:32 -0400

    chore: update ESLint configuration to ignore temporary files

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'view/dist']),
+  globalIgnores(['dist', 'view/dist', '.tmp']),
   {
     files: ['view/**/*.{ts,tsx}'],
     extends: [
