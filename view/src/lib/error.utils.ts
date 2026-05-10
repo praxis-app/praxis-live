53329cac71e5b50acd5c0059e4c4330b772a5f6d
Forrest Wilkins <ethanforrestwilkins@gmail.com>
2026-05-10 12:39:40 -0400

    feat: BE validation for preventing votes on polls in ended calls

--- view/src/lib/error.utils.ts
+++ view/src/lib/error.utils.ts
@@ -4,7 +4,8 @@ import { t } from './shared.utils';
 
 export const handleError = (error: Error) => {
   if (error instanceof AxiosError && error.response?.data) {
-    toast(error.response?.data);
+    const data = error.response.data;
+    toast(typeof data === 'string' ? data : data.error || data.message);
     return;
   }
   toast(error.message || t('errors.somethingWentWrong'));
