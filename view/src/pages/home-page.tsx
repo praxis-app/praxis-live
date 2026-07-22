a1a22f28bc9d0275db9db6395613e5e9e5972c08
Forrest Wilkins <ethanforrestwilkins@gmail.com>
2026-07-22 00:45:22 -0400

    feat: update index title, improve landing page structure

--- view/src/pages/home-page.tsx
+++ view/src/pages/home-page.tsx
@@ -1,8 +1,8 @@
 import { ChannelSkeleton } from '@/components/channels/channel-skeleton';
 import { useAuthData } from '@/hooks/use-auth-data';
 import { useAppStore } from '@/store/app.store';
-import { PublicProductLandingPage } from './public-product-landing-page';
-import { RegisteredHomePage } from './registered-home-page';
+import { PublicProductLandingPage } from './landing/public-product-landing-page';
+import { RegisteredHomePage } from './landing/registered-home-page';
 
 export const HomePage = () => {
   const { isLoggedIn, isMeLoading, isRegistered } = useAuthData();
