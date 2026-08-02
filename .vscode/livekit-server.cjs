966674a58816a87fd28ef053309054701515e5a8
Forrest Wilkins <ethanforrestwilkins@gmail.com>
2026-08-02 15:16:36 -0400

    chore: standardize string quotes and formatting in LiveKit server configuration

--- .vscode/livekit-server.cjs
+++ .vscode/livekit-server.cjs
@@ -1,23 +1,21 @@
-const { spawn } = require("node:child_process");
+const { spawn } = require('node:child_process');
 
-const livekit = spawn(
-  "livekit-server",
-  ["--dev", "--bind", "0.0.0.0"],
-  { stdio: ["inherit", "inherit", "pipe"] },
-);
+const livekit = spawn('livekit-server', ['--dev', '--bind', '0.0.0.0'], {
+  stdio: ['inherit', 'inherit', 'pipe'],
+});
 
 livekit.stderr.pipe(process.stdout);
 
-for (const signal of ["SIGINT", "SIGTERM", "SIGHUP"]) {
+for (const signal of ['SIGINT', 'SIGTERM', 'SIGHUP']) {
   process.once(signal, () => livekit.kill(signal));
 }
 
-livekit.once("error", (error) => {
+livekit.once('error', (error) => {
   console.error(`Unable to start LiveKit Server: ${error.message}`);
   process.exit(1);
 });
 
-livekit.once("exit", (code, signal) => {
+livekit.once('exit', (code, signal) => {
   if (signal) {
     process.kill(process.pid, signal);
   } else {
