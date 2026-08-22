dec6296be81f8d9d71b624747e53063e49b21eff
Forrest Wilkins <ethanforrestwilkins@gmail.com>
2026-08-21 22:44:39 -0400

    refactor: introduce defaults module for admin role constants

--- api/src/authz/types.rs
+++ api/src/authz/types.rs
@@ -1,9 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 
-pub(crate) const ADMIN_ROLE_NAME: &str = "admin";
-pub(crate) const DEFAULT_ROLE_COLOR: &str = "#f44336";
-
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub(crate) struct PermissionRule {
